d3447228c6555d9936effeb32bf7bad04453a8dd
Eva <Eva@EVA>
2026-05-27 09:07:37 -0500

    feat: Correciones server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 
-// Bases de datos
+// Base de datos
 const { sequelize } = require('./config/sql');
 const { connectMongo } = require('./config/nosql');
 
