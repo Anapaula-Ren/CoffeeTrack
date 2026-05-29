4175f516f8a61d32c9525c613092f39aa932f526
Anapaula-Ren <L23100201@nlaredo.tecnm.mx>
2026-05-29 11:30:11 -0500

    Revert "Merge:requerimientos funcionales de nosql"

--- backend/src/services/authService.js
+++ backend/src/services/authService.js
@@ -2,86 +2,38 @@ const bcrypt = require('bcrypt');
 const usuariosModel = require('../models/usuariosModel');
 const { AppError } = require('../middleware/errorHandler');
 
-const LoginLog = require('../models/LoginLog');
-
-console.log('Login');
 class AuthService {
-
   async login(email, password) {
-
     const usuario = await usuariosModel.buscarPorCorreo(email);
-
     if (!usuario) {
       throw new AppError('Credenciales inválidas', 401);
     }
 
     let passwordMatch = false;
 
-    // Validar bcrypt
+    // 1. Intentar validar usando bcrypt
     try {
-
-      passwordMatch = await bcrypt.compare(
-        password,
-        usuario.Contrasena
-      );
-
+      passwordMatch = await bcrypt.compare(password, usuario.Contrasena);
     } catch (error) {
-
       passwordMatch = false;
-
     }
 
-    // Compatibilidad texto plano
+    // 2. Si falla bcrypt, intentar texto plano (soporte híbrido)
     if (!passwordMatch) {
-
-      passwordMatch = (
-        password === usuario.Contrasena
-      );
-
+      passwordMatch = (password === usuario.Contrasena);
     }
 
     if (!passwordMatch) {
       throw new AppError('Credenciales inválidas', 401);
     }
 
-    
-    // LOG MONGODB
-    try {
-
-      await LoginLog.create({
-
-        usuario: usuario.Correo,
-
-        estado: 'LOGIN_EXITOSO',
-
-        ip: 'LOCAL'
-
-      });
-
-    } catch (mongoError) {
-
-    console.log('ERROR MONGO');
-
-    console.log(mongoError);
-
-}
-    console.log('Login guardado');
     return {
-
       IdUsuario: usuario.IdUsuario,
-
       Nombre: usuario.Nombre,
-
       Rol: usuario.Rol,
-
       Correo: usuario.Correo
-
     };
-    
   }
-  
 }
-
-
 
 module.exports = new AuthService();
